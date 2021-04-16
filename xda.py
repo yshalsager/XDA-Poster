25abd64e253a771978f6f9233be194fe967e8f12
yshalsager <ysh-alsager@hotmail.com>
2021-04-16 16:19:27 +0200

    Add update posts method
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- xda.py
+++ xda.py
@@ -4,8 +4,8 @@ SPDX-License-Identifier: GPL-3.0
 authors: @baalajimaestro, @yshalsager
 """
 
-from requests import post
 from aiohttp import ClientSession
+from requests import post
 
 
 class XDA:
@@ -31,6 +31,18 @@ class XDA:
         if not xda_req.status_code == 200:
             print(f"XDA Error: {xda_req.reason}")
 
+    def update_post(self, post_id, message):
+        """
+        Update an XDA post
+        :param message: post text
+        :type message: str
+        :param post_id: XDA post id
+        :type post_id: int
+        """
+        xda_req = post(f'{self.url}/posts/{post_id}', data={"message": message}, headers=self.headers)
+        if not xda_req.status_code == 200:
+            print(f"XDA Error: {xda_req.reason}")
+
     async def post_async(self, thread_id, message):
         """
         Post XDA post to a thread asynchronously
@@ -42,6 +54,21 @@ class XDA:
         async with ClientSession() as session:
             async with session.post(
                     f'{self.url}/posts', data={"thread_id": thread_id, "message": message},
+                    headers=self.headers) as resp:
+                if not resp.status == 200:
+                    print(f"XDA Error: {resp.reason}")
+
+    async def update_post_async(self, post_id, message):
+        """
+        Update an XDA post asynchronously
+        :param message: post text
+        :type message: str
+        :param post_id: XDA thread id
+        :type post_id: int
+        """
+        async with ClientSession() as session:
+            async with session.post(
+                    f'{self.url}/posts/{post_id}', data={"message": message},
                     headers=self.headers) as resp:
                 if not resp.status == 200:
                     print(f"XDA Error: {resp.reason}")
