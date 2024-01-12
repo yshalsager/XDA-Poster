e5e749b532243a64ef627afeb80fac55e5d62d55
yshalsager <ysh-alsager@hotmail.com>
2024-01-12 20:16:22 +0200

    refactor: switch to httpx
    * use http 2 since XDA has blocked http 1 traffic
    
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- xda.py
+++ xda.py
@@ -4,14 +4,13 @@ SPDX-License-Identifier: GPL-3.0
 authors: @baalajimaestro, @yshalsager
 """
 
-from aiohttp import ClientSession
-from requests import post
+from httpx import AsyncClient, Response, post
 
 
 class XDA:
     """XDA API client"""
 
-    def __init__(self, api_key):
+    def __init__(self, api_key: str):
         self.url = "https://xdaforums.com/api"
         self.api_key = api_key
         self.headers = {
@@ -19,56 +18,48 @@ class XDA:
             'Content-Type': 'application/x-www-form-urlencoded'
         }
 
-    def post(self, thread_id, message):
+    def post(self, thread_id: int, message: str):
         """
         Post XDA post to a thread
         :param message: reply text
-        :type message: str
         :param thread_id: XDA thread id
-        :type thread_id: int
         """
         xda_req = post(f'{self.url}/posts', data={"thread_id": thread_id, "message": message}, headers=self.headers)
         if not xda_req.status_code == 200:
-            print(f"XDA Error: {xda_req.reason}\nResponse: {xda_req.text}")
+            print(f"XDA Error: {xda_req.reason_phrase}\nResponse: {xda_req.text}")
 
-    def update_post(self, post_id, message):
+    def update_post(self, post_id: int, message: str):
         """
         Update an XDA post
         :param message: post text
-        :type message: str
         :param post_id: XDA post id
-        :type post_id: int
         """
         xda_req = post(f'{self.url}/posts/{post_id}', data={"message": message}, headers=self.headers)
         if not xda_req.status_code == 200:
-            print(f"XDA Error: {xda_req.reason}\nResponse: {xda_req.text}")
+            print(f"XDA Error: {xda_req.reason_phrase}\nResponse: {xda_req.text}")
 
-    async def post_async(self, thread_id, message):
+    async def post_async(self, thread_id: int, message: str):
         """
         Post XDA post to a thread asynchronously
         :param message: reply text
-        :type message: str
         :param thread_id: XDA thread id
-        :type thread_id: int
         """
-        async with ClientSession() as session:
-            async with session.post(
-                    f'{self.url}/posts', data={"thread_id": thread_id, "message": message},
-                    headers=self.headers) as resp:
-                if not resp.status == 200:
-                    print(f"XDA Error: {resp.reason}\nResponse: {await resp.text()}")
+        async with AsyncClient() as client:
+            resp: Response = await client.post(
+                f'{self.url}/posts', data={"thread_id": thread_id, "message": message},
+                headers=self.headers)
+            if not resp.status_code == 200:
+                print(f"XDA Error: {resp.reason_phrase}\nResponse: {resp.text}")
 
-    async def update_post_async(self, post_id, message):
+    async def update_post_async(self, post_id: int, message: str):
         """
         Update an XDA post asynchronously
         :param message: post text
-        :type message: str
         :param post_id: XDA thread id
-        :type post_id: int
         """
-        async with ClientSession() as session:
-            async with session.post(
-                    f'{self.url}/posts/{post_id}', data={"message": message},
-                    headers=self.headers) as resp:
-                if not resp.status == 200:
-                    print(f"XDA Error: {resp.reason}\nResponse: {await resp.text()}")
+        async with AsyncClient() as client:
+            resp: Response = await client.post(
+                f'{self.url}/posts/{post_id}', data={"message": message},
+                headers=self.headers)
+            if not resp.status_code == 200:
+                print(f"XDA Error: {resp.reason_phrase}\nResponse: {resp.text}")
